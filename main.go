270f29b313aa9b9178aa40b01153a1501ca3760b
bhagyashree-tiwari <reachbhagyashree.tiwari@gmail.com>
2023-09-26 12:41:17 +0530

    final code with test files

--- main.go
+++ main.go
@@ -1,76 +1,131 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"strings"
 	"time"
 )
 
+// Number of Questions to ask
+const totalQuestions = 10
+
+// Question struct that stores question with answer
+type Question struct {
+	question string
+	answer   string
+}
+
 func main() {
-	csvFileName := flag.String("csv", "problems.csv", "a csv file format in the form of 'questions, answers'")
-	timeLimit := flag.Int("limit", 30, "Default time limit for the quiz in seconds")
+	filename, timeLimit := readArguments()
+	f, err := openFile(filename)
+	if err != nil {
+		return
+	}
+	questions, err := readCSV(f)
+
+	if err != nil {
+		// err := fmt.Errorf("Error in Reading Questions")
+		fmt.Println(err.Error())
+		return
+	}
+
+	if questions == nil {
+		return
+	}
+	score, err := askQuestion(questions, timeLimit)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("\nYour Score %d/%d\n", score, totalQuestions)
+}
+
+func readArguments() (string, int) {
+	filename := flag.String("filename", "problem.csv", "CSV File that conatins quiz questions")
+	timeLimit := flag.Int("limit", 30, "Time Limit for each question")
 	flag.Parse()
-	// _ = csvFileName //code compiling temporarily
+	return *filename, *timeLimit
+}
 
-	// Reading the file
-	file, err := os.Open(*csvFileName)
+func readCSV(f io.Reader) ([]Question, error) {
+	// defer f.Close() // this needs to be after the err check
+	allQuestions, err := csv.NewReader(f).ReadAll()
 	if err != nil {
-		exit(fmt.Sprintf("failed to open the CSV file: %s\n", *csvFileName))
+		return nil, err
 	}
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
-	if err != nil {
-		exit("Failed to parse the provided CSV file")
+
+	numOfQues := len(allQuestions)
+	if numOfQues == 0 {
+		return nil, fmt.Errorf("no question in file")
 	}
-	problems := parseLines(lines)
 
-	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
-	// <-timer.C //waiting for message from that channel
-	correct := 0
-	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		answerCh := make(chan string)
-		go func() {
-			var answer string
-			fmt.Scanf("%s\n", &answer)
-			answerCh <- answer
+	var data []Question
+	for _, line := range allQuestions {
+		ques := Question{}
+		ques.question = line[0]
+		ques.answer = line[1]
+		data = append(data, ques)
+	}
 
-		}()
+	return data, nil
+}
+
+func openFile(filename string) (io.Reader, error) {
+	return os.Open(filename)
+}
+func getInput(input chan string) {
+	for {
+		in := bufio.NewReader(os.Stdin)
+		result, err := in.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input <- result
+	}
+}
+
+func askQuestion(questions []Question, timeLimit int) (int, error) {
+	totalScore := 0
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+	done := make(chan string)
+
+	go getInput(done)
+
+	for i := range [totalQuestions]int{} {
+		ans, err := eachQuestion(questions[i].question, questions[i].answer, timer.C, done)
+		if err != nil && ans == -1 {
+			return totalScore, nil
+		}
+		totalScore += ans
+
+	}
+	return totalScore, nil
+}
+
+func eachQuestion(Quest string, answer string, timer <-chan time.Time, done <-chan string) (int, error) {
+	fmt.Printf("%s: ", Quest)
+
+	for {
 		select {
-		case <-timer.C:
-			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
-			return
-		case answer := <-answerCh:
-			if answer == p.a {
-				correct++
+		case <-timer:
+			return -1, fmt.Errorf("time out")
+		case ans := <-done:
+			score := 0
+			if strings.Compare(strings.Trim(strings.ToLower(ans), "\n"), answer) == 0 {
+				score = 1
+			} else {
+				return 0, fmt.Errorf("wrong Answer")
 			}
+
+			return score, nil
 		}
 	}
-	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
-}
-
-func parseLines(lines [][]string) []problem {
-	// return a slice of problem
-	ret := make([]problem, len(lines))
-	for i, line := range lines {
-		ret[i] = problem{
-			q: line[0],
-			a: strings.TrimSpace(line[1]),
-		}
-	}
-
-	return ret
-}
-
-type problem struct {
-	q string
-	a string
-}
-
-func exit(msg string) {
-	fmt.Println(msg)
-	os.Exit(1)
 }
